5e58103e5faf68f7f631caab4331c9ed7d068b0c
chi.dev <makhortykh.ruslan@gmail.com>
2026-01-28 22:34:22 +0200

    REFACTORING: change logo URL

--- src/core/components/Logo/Logo.tsx
+++ src/core/components/Logo/Logo.tsx
@@ -1,5 +1,6 @@
-import { Icon } from '@/src/core/components/Icon/Icon';
+import Image from 'next/image';
 
+import logo from '../../../../public/images/logo.png';
 import { getLogoStyle } from './Logo.style';
 
 export const Logo = () => {
@@ -7,7 +8,7 @@ export const Logo = () => {
 
   return (
     <div className={styles.component}>
-      <Icon name="cloud" className={styles.component_logo}></Icon>
+      <Image src={logo} width={40} height={40} alt="logo" />
     </div>
   );
 };
